774e83fada67b4e687a14874c9f126f7ce12562b
pcoombe <patrick.ryan.coombe@gmail.com>
2025-03-14 18:42:04 -0500

    scripts initial

---
+++ target-scripts/target-pokemon-prismatic-acc-pouch.py
@@ -0,0 +1,22 @@
+from target_webbot import *
+
+LOG_FILE = rf"C:\Logs\selenium-webbot\target-pokemon-prismatic-acc-pouch.csv"
+PRODUCT_URL = "https://www.target.com/p/2025-pokemon-prismatic-evolutions-accessory-pouch-special-collection/-/A-94300053"
+
+configure_logging(LOG_FILE)
+driver = setup_driver()
+try:
+    #send_sms_twilio(PRODUCT_URL)
+    sign_in(driver)
+    if add_to_cart(driver, PRODUCT_URL):
+        if place_order(driver):  # ensure order is placed before retrying
+            logging.info("first purchase successful. running a second attempt.")
+            time.sleep(5)  # short pause before re-running
+            
+            if add_to_cart(driver, PRODUCT_URL):
+                place_order(driver)
+                logging.info("second purchase successful.")
+finally:
+    driver.quit()
+    logging.info("WebDriver session ended.")
+
